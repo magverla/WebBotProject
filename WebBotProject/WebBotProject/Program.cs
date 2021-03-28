e71d1c75e5e5bc73980aeb302dac69f9ab175bb2
magverla <magverla@gmail.com>
2021-03-28 21:48:00 +0200

    first commit

---
+++ WebBotProject/WebBotProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebBotProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
